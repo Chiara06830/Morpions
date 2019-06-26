92d17ae77e80c5712aa9ca87c9fe5652cac819ab
Chiara <chiara.relevat@lilo.org>
2019-06-26 16:21:01 +0200

    Ajout classe joueur

---
+++ Morpion/src/programme/Joueur.java
@@ -0,0 +1,19 @@
+package programme;
+
+public class Joueur {
+	//ATTRIBUTS 
+	private String nom;
+	private boolean couleur;
+	
+	//CONSTRUCTEURS 
+	public Joueur (String nom, boolean couleur) {
+		this.nom = nom;
+		this.couleur = couleur;
+	}
+	
+	//GETTERS AND SETTERS
+	public String getNom() {return nom;}
+	public void setNom(String nom) {this.nom = nom;}
+	public boolean isCouleur() {return couleur;}
+	public void setCouleur(boolean couleur) {this.couleur = couleur;}
+}
