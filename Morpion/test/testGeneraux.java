82ef4580b5cc6f110411b7c1907576ad9af3bf6c
Chiara <chiara.relevat@lilo.org>
2019-06-29 12:30:16 +0200

    création classe test
    actuellement vides

---
+++ Morpion/test/testGeneraux.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import programme.*;
+
+class testGeneraux {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Morpion/test/testVictoire.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import programme.*;
+
+class testVictoire {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
